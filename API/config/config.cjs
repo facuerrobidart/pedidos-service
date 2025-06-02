dfebbfdfcaaeba0c2fb43c7fc0635f6209f37132
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2025-06-01 21:54:06 -0300

    add npm start

--- API/config/config.cjs
+++ API/config/config.cjs
@@ -2,11 +2,11 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    database: process.env.DB_NAME || 'pedidos_db',
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || '5432',
     dialect: "postgres"
   },
   // You can add test/production environments similarly
